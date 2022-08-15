7c1df02a1952d09a46c2436894c39c41aa238696
jm-moore <jmmoore9358@gmail.com>
2022-08-15 15:03:29 -0500

    initial commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,7 @@
+﻿using MethodOverloading;
+
+methods.Add(4.45, 5.78);
+
+methods.Add(5, 6);
+
+methods.Add(12, 15, false);
---
+++ MethodOverloading/methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Add(double num3, double num4)
+        {
+            return num3 + num4;
+        }
+
+        public static string Add(int num5, int num6, bool maybe)
+        {
+            int sum = num5 + num6;
+            if (maybe)
+            {
+                if (sum > 1 && sum < 2)
+                {
+                    return $"{sum} dollar.";
+                }
+                else if (sum > 2)
+                {
+                    return $"{sum} dollars.";
+                }
+            }
+            return $"{sum}";
+        }
+    }
+}
